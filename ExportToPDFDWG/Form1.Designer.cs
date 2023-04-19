0bfbae5d76aa0e59f5c4f68ad61b9cb67358645b
idlanAzahari <idlanAzahari@cadcam.com.my>
2023-04-19 11:11:20 +0800

    Add project files.

---
+++ ExportToPDFDWG/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace ExportToPDFDWG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pdf = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ExportDWG = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pdf
+            // 
+            this.pdf.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pdf.Location = new System.Drawing.Point(66, 135);
+            this.pdf.Name = "pdf";
+            this.pdf.Size = new System.Drawing.Size(161, 42);
+            this.pdf.TabIndex = 0;
+            this.pdf.Text = "Export to PDF";
+            this.pdf.UseVisualStyleBackColor = false;
+            this.pdf.Click += new System.EventHandler(this.pdf_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Location = new System.Drawing.Point(66, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Print PDF";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.print_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button4.Location = new System.Drawing.Point(66, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ExportDWG
+            // 
+            this.ExportDWG.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ExportDWG.Location = new System.Drawing.Point(66, 195);
+            this.ExportDWG.Name = "ExportDWG";
+            this.ExportDWG.Size = new System.Drawing.Size(161, 43);
+            this.ExportDWG.TabIndex = 4;
+            this.ExportDWG.Text = "Export To DWG";
+            this.ExportDWG.UseVisualStyleBackColor = false;
+            this.ExportDWG.Click += new System.EventHandler(this.ExportDWG_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "*Note that after export to PDF or DWG, make sure to close SolidWorks before expor" +
+    "ting again.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "*Note that the form is using SolidWorks 2022 SP5";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(66, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Print DWG";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(502, 452);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExportDWG);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pdf);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IME Export To DWG and PDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pdf;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button ExportDWG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ExportToPDFDWG/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+using eDrawings.Interop.EModelViewControl;
+
+namespace ExportToPDFDWG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Instantiate the OpenFileDialog class
+            FolderBrowserDialog openFileDialog = new FolderBrowserDialog();
+
+
+            
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = openFileDialog.SelectedPath;
+                button4.Tag = folderPath;
+            }
+
+        }
+
+        private void pdf_Click(object sender, EventArgs e)
+        {
+            //tak jadi lagi fck
+            string path = (string)button4.Tag;
+
+            if (path != null)
+            {
+                SldWorks swApp = null;
+                ModelDoc2 swModel = null;
+                DirectoryInfo folder = new DirectoryInfo(path);
+                string pdfFolder = "PDF folder";
+                string pdfFolderPath = Path.Combine(path, pdfFolder);
+                Directory.CreateDirectory(pdfFolderPath);
+
+                foreach (FileInfo file in folder.GetFiles())
+                {
+                    
+                    string filePath = path + "\\" + file;
+                    string fileName = pdfFolderPath + "\\" + file.Name.Replace(".SLDDRW", ".PDF");
+
+                    //open the file in solidworks
+                    swApp = (SldWorks)Activator.CreateInstance(Type.GetTypeFromProgID("SldWorks.Application"));
+                    swModel = (ModelDoc2)swApp.OpenDoc6(filePath, (int)swDocumentTypes_e.swDocDRAWING, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", 0, 0);
+                    swApp.Visible = true;
+
+                    //check if solidwork file is null or not
+                    if (swModel != null && swModel.GetType() == (int)swDocumentTypes_e.swDocDRAWING)
+                    {
+                        swModel.SaveAs4(fileName, (int)swSaveAsVersion_e.swSaveAsCurrentVersion, (int)swSaveAsOptions_e.swSaveAsOptions_Silent, 0, 0);
+
+                    }
+                    else
+                        continue;
+
+                    
+                }
+                MessageBox.Show("All files saved successfully to PDF");
+            }
+            else
+                MessageBox.Show("Please browse a folder");
+
+
+        }
+
+        private void ExportDWG_Click(object sender, EventArgs e)
+        {
+            string path = (string)button4.Tag;
+
+            if (path != null)
+            {
+                SldWorks swApp = null;
+                ModelDoc2 swModel = null;
+                DirectoryInfo folder = new DirectoryInfo(path);
+                string pdfFolder = "DWG folder";
+                string pdfFolderPath = Path.Combine(path, pdfFolder);
+                Directory.CreateDirectory(pdfFolderPath);
+
+                foreach (FileInfo file in folder.GetFiles())
+                {
+
+                    string filePath = path + "\\" + file;
+                    string fileName = pdfFolderPath + "\\" + file.Name.Replace(".SLDDRW", ".DWG");
+
+                    //open the file in solidworks
+                    swApp = (SldWorks)Activator.CreateInstance(Type.GetTypeFromProgID("SldWorks.Application"));
+                    swModel = (ModelDoc2)swApp.OpenDoc6(filePath, (int)swDocumentTypes_e.swDocDRAWING, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", 0, 0);
+                    swApp.Visible = true;
+
+                    //check if solidwork file is null or not
+                    if (swModel != null && swModel.GetType() == (int)swDocumentTypes_e.swDocDRAWING)
+                    {
+                        swModel.SaveAs4(fileName, (int)swSaveAsVersion_e.swSaveAsCurrentVersion, (int)swSaveAsOptions_e.swSaveAsOptions_Silent, 0, 0);
+
+                    }
+                    else
+                        continue;
+
+
+                }
+                MessageBox.Show("All files saved successfully to PDF");
+            }
+            else
+                MessageBox.Show("Please browse a folder");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            string path = (string)button4.Tag;
+            string pdfFolderPath = path + "\\" + "PDF folder";
+            DirectoryInfo pdfFolder = new DirectoryInfo(pdfFolderPath);
+
+            foreach(FileInfo file in pdfFolder.GetFiles())
+            {
+                
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //planning to 
+            
+
+
+
+
+        }
+    }
+
+
+
+
+}
